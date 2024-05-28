a1c489ba2ec251cd03c6875427738c5953318916
leneholstt <lene7315@icloud.com>
2024-05-28 13:11:21 +0200

    Update popup.js

--- JS/popup.js
+++ JS/popup.js
@@ -1,16 +1,27 @@
 function myFunction() {
     let popup = document.getElementById("myPopup");
     let popupText = document.getElementById("popupText");
-
     
     let messages = ["Medarbejder tilgængelig", "5 minutters ventetid"];
     let randomMessage = messages[Math.floor(Math.random() * messages.length)];
     popupText.textContent = randomMessage;
 
-    popup.classList.toggle("show");
+    
+    if (!popup.classList.contains("show")) {
+        popup.classList.add("show");
+    } else {
+        popup.classList.remove("show");
+    }
 }
+
 
 function closePopup() {
     let popup = document.getElementById("myPopup");
     popup.classList.remove("show");
+}
+
+
+let popupTriggers = document.querySelectorAll(".popup-trigger");
+for (let i = 0; i < popupTriggers.length; i++) {
+    popupTriggers[i].addEventListener("click", myFunction);
 }
