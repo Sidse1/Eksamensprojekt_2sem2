3e316030734e9bc9d478af51eed980d3fae8508e
leneholstt <lene7315@icloud.com>
2024-05-28 09:50:30 +0200

    Create menu.js

---
+++ JS/menu.js
@@ -0,0 +1,32 @@
+let sidebarWidth = 250;
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = sidebarWidth + "px";
+  document.getElementById("main2").style.marginRight = sidebarWidth + "px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main2").style.marginRight = "0";
+}
+
+let sidebarOpen = false;
+
+function toggleNav() {
+  if (sidebarOpen) {
+    closeNav();
+    sidebarOpen = false;
+  } else {
+    openNav();
+    sidebarOpen = true;
+  }
+}
+
+document.querySelector('.openbtn').addEventListener('click', toggleNav);
+
+let buttons = document.querySelectorAll('.sidebar a');
+
+buttons.forEach(button => {
+  button.addEventListener('click', closeNav);
+});
+
