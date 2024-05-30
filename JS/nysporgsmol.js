c5ca235fcba49ba3f66d4688b53e27fae965b6c7
leneholstt <lene7315@icloud.com>
2024-05-30 09:42:46 +0200

    Update nysporgsmol.js

--- JS/nysporgsmol.js
+++ JS/nysporgsmol.js
@@ -1,17 +1,19 @@
-let products = ["Lampe", "Stikkontakt", "Kabel", "Elektrisk skruetrækker", "Sikringsboks"];
-    let select = document.getElementById("productList");
+let products = ["El-artikler", "Belysning", "tavlemateriale", "LK", "andet"];
+let select = document.getElementById("productList");
 
-    if (products.length > 0) {
-        products.forEach(function(product) {
-            let option = document.createElement("option");
-            option.text = product;
-            option.value = product.toLowerCase().replace(/\s+/g, "-");
-            select.add(option);
-        });
-    } else {
-        console.log("No products available");
+if (products.length > 0) {
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+        let option = document.createElement("option");
+        option.text = product;
+        option.value = product.toLowerCase().replace(/\s+/g, "-");
+        select.add(option);
     }
+} else {
+    console.log("No products available");
+}
 
-    select.addEventListener('click', function() {
-        console.log("Select element clicked");
-    });
+select.addEventListener('click', function() {
+    console.log("Select element clicked");
+});
+
